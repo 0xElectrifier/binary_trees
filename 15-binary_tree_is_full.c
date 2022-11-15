e7594d6bc1816882bac0b3dcb975c61401f339d3
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-11-15 17:28:03 +0100

    Changed the implementation of code after better understanding a 'full tree'

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,64 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- *
- * Return: height of tree or 0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t  height_left, height_right;
-
-	if (tree == NULL)
-		return (0);
-
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-	if (height_left >= height_right)
-		return (height_left + 1);
-	else
-		return (height_right + 1);
-}
-
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- *
- * Return: size of @tree or 0 if @tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t size = 0;
-
-	if (tree == NULL)
-		return (size);
-
-	size += binary_tree_size(tree->left);
-	size += binary_tree_size(tree->right);
-
-	return (size + 1);
-}
-
-/**
- * _pow_recursion - function that returns the value of x raised to the power
- * of y
- * @x: first integer parameter
- * @y: second integer parameter
- *
- * Return: x raised to the power of y
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	else
-		return (x * _pow_recursion(x, y - 1));
-}
-
-/**
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: pointer to the root node of the tree to check
  *
@@ -66,17 +8,26 @@ int _pow_recursion(int x, int y)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	double tree_height, tree_size, complete_size;
+	int ret_left, ret_right;
 
 	if (tree == NULL)
 		return (0);
 
-	tree_height = binary_tree_height(tree);
-	tree_size = binary_tree_size(tree);
-
-	complete_size = _pow_recursion(2, tree_height) - 1;
-	if (complete_size == tree_size)
+	if ((tree->left == NULL) && (tree->right == NULL))
 		return (1);
+
+	if ((tree->left != NULL) && (tree->right != NULL))
+	{
+		ret_left = binary_tree_is_full(tree->left);
+		/*
+		 * The 'if statement' makes sure a recursion doesn't start
+		 * if ret_left == 0, to speed up performance
+		 */
+		if (ret_left != 0)
+			ret_right = binary_tree_is_full(tree->right);
+
+		return (ret_left && ret_right);
+	}
 	else
 		return (0);
 }
