19cc914574d9ea0b3632704b383ae358db635fb2
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-18 04:33:11 +0100

    Done

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,12 @@
 #ifndef __BINARY_TREE_H__
 #define __BINARY_TREE_H__
 
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdbool.h>
 
 
 /**
