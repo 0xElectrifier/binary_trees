e1d62d9747a53b6e9776c53ffab7a5250174d9e4
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-18 11:45:16 +0100

    Added comments and resolved betty error

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * root_traverse - traverses the tree from root searching for two nodes
+ * root_traverse - traverses the tree from root searching for the two nodes
  * @current: pointer to the current tree to traverse
- * @firsr: pointer to the first node
+ * @first: pointer to the first node
  * @second: pointer to the second node
  *
  * Return: pointer to the found node, or NULL if non is found
@@ -14,17 +14,28 @@ binary_tree_t *root_traverse(const binary_tree_t *current,
 {
 	binary_tree_t *left, *right;
 
+	/* Check if current node is NULL */
 	if (current == NULL)
 		return (NULL);
 
+	/* Check if current node is same as first or second */
 	if (current == first || current == second)
-		return (binary_tree_t *)(current);
+		return ((binary_tree_t *)(current));
 
+	/*
+	 * Traverse the two child nodes, checking for the presence of
+	 * either of first or second
+	 */
 	left = root_traverse(current->left, first, second);
 	right = root_traverse(current->right, first, second);
 
+	/*
+	 * if left and right is a Non-NULL value return the current node
+	 * if left or right is a Non-NULL value, return it
+	 * if left and right are both NULL value, return NULL
+	 */
 	if (left && right)
-		return (binary_tree_t *)(current);
+		return ((binary_tree_t *)(current));
 	else if (left)
 		return (left);
 	else if (right)
@@ -50,13 +61,15 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	if (!first || !second)
 		return (NULL);
 
+	/* Track the root node of both nodes */
 	while (first_root->parent)
 		first_root = first_root->parent;
 	while (second_root->parent)
 		second_root = second_root->parent;
 
+	/* Compare them to know if they belong to the same sub-tree */
 	if (first_root == second_root)
-		return(root_traverse(first_root, first, second));
+		return (root_traverse(first_root, first, second));
 
 	return (NULL);
 }
